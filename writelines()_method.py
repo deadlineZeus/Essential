b60bf273039ce823356c322c2c20d89abfcf5b28
deadlineZeus <132813431+deadlineZeus@users.noreply.github.com>
2024-05-15 13:45:11 +0530

    Delete writelines()_method.py

--- writelines()_method.py
+++
@@ -1,9 +0,0 @@
-# if a variable holds collection of strings, then we can write each element in a file
-# using writeline() method
-
-f = open('C:/Users/Rajdeep Ray/Documents/College Docs/WriteLines.txt', mode='w')
-collection = ['Rajdeep', 'Anubhab', 'Lokesh', 'Sagarika', 'Bijay']
-
-f.writelines(collection)   # will write one each tem after another
-
-f.close()
